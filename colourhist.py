5c55f3462320892d9aebd3849fe99bad86e57c68
DAVID LORENZO ALFARO <David.Lorenzo@alu.uclm.es>
2022-03-12 18:44:03 +0100

    comentado métodos + fixed minor bugs

--- colourhist.py
+++ colourhist.py
@@ -43,9 +43,9 @@ def makedir(path:str, remove_filename=False, recursive=True, exist_ok=True):
         path = path if not remove_filename else remove_filename_from_path(path)
         Path(path).mkdir(parents=recursive, exist_ok=exist_ok)
 
-def load_embeddings(filename:str, return_dict_values=False):
-    """Utility to load embeddings (and other optional stored values) from disk
-    using *pickle*.
+def load_serialized_data(filename:str, return_dict_values=False):
+    """Utility to load serialized data (and other optional stored values)
+    from disk using *pickle*.
     
     :param str filename: Filename of the file to be loaded.
     :param return_dict_values: If set to True, returns the values just the values
@@ -58,14 +58,14 @@ def load_embeddings(filename:str, return_dict_values=False):
         stored_data = pickle.load(fIn)
     return stored_data.values() if return_dict_values else stored_data
 
-def store_embeddings(corpus_embeddings, out_filename='embeddings.pkl',
+def store_serialized_data(corpus_embeddings, out_filename='colour_embeddings.pkl',
         protocol=pickle.HIGHEST_PROTOCOL, **kwargs):
     """Utility to dump embeddings (and other optional values indicated in the 
     keyword arguments) to disk using *pickle*.
 
-    :param corpus_embeddings: Tensor type data structure containing the embeddings
-     for the corpus.
-    :param out_filename: Path for the output file, defaults to 'embeddings.pkl'.
+    :param corpus_embeddings: Tensor type data structure containing the dominant
+     colours of the dataset
+    :param out_filename: Path for the output file, defaults to 'colour_embeddings.pkl'.
     :type out_filename: str, optional
     :param protocol: Protocol used for *pickle*, defaults to `pickle.HIGHEST_PROTOCOL`.
     """
@@ -75,12 +75,36 @@ def store_embeddings(corpus_embeddings, out_filename='embeddings.pkl',
         pickle.dump(corpus_embeddings, fOut, protocol=protocol)
 
             
-def load_files_from_dirs_to_mm(*pattern_dirs, file_extension="jpg"):
-    return np.array([f for directory in pattern_dirs for f in glob2.glob(directory + "\\*." + file_extension)])
+def load_files_from_dirs_to_mm(fdir, file_extension="jpg"):
+    """ Scan files recursively from a directory with a specific file extension.
 
+    :param fdir: Source directory.
+    :param out_filename: File extension, defaults to 'jpg'.
+    :type out_filename: str, optional
+    """
+    return np.array([os.path.join(dirname, filename)
+                        for dirname, _, filenames in os.walk(fdir) 
+                            for filename in filenames 
+                                if filename.endswith(f".{file_extension}")
+                    ]
+                   )
 
 def _get_fig_layout(n_graphs, gs_x=5, gs_y=5, ncols=None):
+    """Get optimal plot layout for `n_graphs` subplots.
+
+    :param n_graphs: Number of subplots
+    :type n_graphs: int
+    :param gs_x: Size of the subplots in the x axis, defaults to 5
+    :type gs_x: int, optional
+    :param gs_y: Size of the subplots in the y axis, defaults to 5
+    :type gs_y: int, optional
+    :param ncols: Number of columns of the plot, defaults to None
+    :type ncols: int, optional
+    :return: Figure layout
+    """
+    # If the number of columns is not fixed
     if not ncols:
+        # obtain a square layout
         sqrt = math.sqrt(n_graphs)
         ncols = sqrt + 1 if sqrt - int(sqrt) > 0 else sqrt
         nrows = ncols
@@ -90,13 +114,23 @@ def _get_fig_layout(n_graphs, gs_x=5, gs_y=5, ncols=None):
         
     return (nrows, ncols), plt.figure(figsize=(gs_x*ncols, gs_y*nrows))
 
-def plot_img_grid(data, **kwargs):
+def plot_img_grid(data, img_like=False, col_space_conv=None, **kwargs):
+    """Plot grid of data
+
+    :param data: iterable of filepaths to images or plots
+    :type data: iterable
+    """
     grid_dim, fig = _get_fig_layout(len(data), **kwargs)
     for i, img in enumerate(data):
+        if not img_like:
+            img = cv2.imread(img)
         num = 1 + i
         ax = fig.add_subplot(*grid_dim, num)
         ax.axis('off')
-        ax.imshow(cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB))
+        if col_space_conv:
+            ax.imshow(cv2.cvtColor(img, col_space_conv))
+        else:
+            ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     plt.tight_layout()
     plt.show()
 
@@ -132,6 +166,21 @@ def rgb_hist(image, size_hist=256, showRes=True):
     return histImage
 
 def dominant_colours(img, n_colours=5, show_palette=False):
+    """Compute the `n_colours` most representative colours of
+    an image located in the path `img`.
+
+    :param img: Path to an image
+    :type img: str
+    :param n_colours: Number of representative colours to get 
+     from the image, defaults to 5
+    :type n_colours: int, optional
+    :param show_palette: show the resultant colour palette, 
+     defaults to False
+    :type show_palette: bool, optional
+    :return: Collection of dominant colours and their frequence
+     of appearance.
+    """
+    # Read image using the l*a*b colour space.
     img = cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2LAB)
     pixels = np.float32(img.reshape(-1, 3))
 
@@ -142,22 +191,32 @@ def dominant_colours(img, n_colours=5, show_palette=False):
     _, counts = np.unique(labels, return_counts=True)
 
     indices = np.argsort(counts)[::-1]   
-    freqs = [counts[indices]/float(counts.sum())]
+    freqs = np.array([counts[indices]/float(counts.sum())])
 
     if show_palette:
-        cum_freq = np.cumsum(np.hstack([0], cum_freq))
+        cum_freq = np.cumsum(np.hstack(([0], np.squeeze(freqs))))
         rows = np.int_(img.shape[0]*cum_freq)
         dom_patch = np.zeros(shape=img.shape, dtype=np.uint8)
         for i in range(len(rows) - 1):
             dom_patch[rows[i]:rows[i + 1], :, :] += np.uint8(palette[indices[i]])
-        cv2.imshow('Dominant colors', dom_patch)
-        cv2.waitKey()
+        plot_img_grid([img, dom_patch], img_like=True, 
+                       col_space_conv=cv2.COLOR_LAB2RGB, ncols=1)
+        # cv2.imshow('Dominant colors', dom_patch)
+        # cv2.waitKey()
     
     return palette, freqs
 
 def serialize_dominant_colours(dataset, filename="colour_embeddings.pkl", **kwargs):
+    """Serialize the computed dominant colours of the images in a dataset.
+
+    :param dataset: dataset of images.
+    :type dataset: iterable collection of images.
+    :param filename: output filepath, defaults to "colour_embeddings.pkl"
+    :type filename: str, optional
+    :return: collection of dominant colours and their frequence 
+    """
     dic = {file:dominant_colours(file, **kwargs) for file in dataset}
-    store_embeddings(dic, filename)
+    store_serialized_data(dic, filename)
     return dic
 
 def resize_img(img, width=250, height=None):
@@ -182,7 +241,7 @@ def colour_img_score(colour, palette):
 
 def search_by_colour(colour, dataset, serial_dc=None, topk=10):
     if serial_dc:
-        dc = load_embeddings(serial_dc)
+        dc = load_serialized_data(serial_dc)
         # Override existing definition of dominant colours extraction.
         dominant_colours = lambda img: dc[img]
 
@@ -200,7 +259,7 @@ color = cv2.cvtColor(b, cv2.COLOR_BGR2LAB)
 dir_origin = 'pokemon_dataset\\'
 serial_path = 'colour_embeddings.pkl'
 
-dc = load_embeddings(serial_path, return_dict_values=False)
+dc = load_serialized_data(serial_path, return_dict_values=False)
 # for dirname, _, filenames in os.walk(dir_origin):
 #     pokemon = dirname.split('/')
 #     print(pokemon)
@@ -213,27 +272,29 @@ dataset = np.array([os.path.join(dirname, filename)
 
 # serialize_dominant_colours(dataset)
 
-# Yellow
-search_c = np.array([232, 118, 217])
+# # Yellow
+# search_c = np.array([232, 118, 217])
 
 # Purple
-search_c = np.array([168, 158, 88])
+# search_c = np.array([168, 158, 88])
 
-# Orange
-search_c = np.array([168, 167, 199])
+# # Orange
+# search_c = np.array([168, 167, 199])
 
 # Pink
 search_c = np.array([226, 141, 132])
 
-# Blue
-search_c = np.array([191, 118, 101])
+# # Blue
+# search_c = np.array([191, 118, 101])
 
+SAMPLE_IMAGE = "pokemon_dataset\\Abra\\10a9f06ec6524c66b779ea80354f8519.jpg"
+dominant_colours(SAMPLE_IMAGE, show_palette=True)
 
-img = cv2.cvtColor(cv2.imread("colours\\lila.jpg"), cv2.COLOR_BGR2LAB)
+# img = cv2.cvtColor(cv2.imread("colours\\lila.jpg"), cv2.COLOR_BGR2LAB)
 
-results = search_by_colour(search_c, dataset, serial_dc=serial_path)
+# results = search_by_colour(search_c, dataset, serial_dc=serial_path)
 
-plot_img_grid(results)
+# plot_img_grid(results)
 
 
 # rimg = resize_img(src)
